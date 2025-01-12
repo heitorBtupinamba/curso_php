090bd7cda90e2754944da8414171d9029482694a
Heitor Botelho <heitorbotelhotupi@gmail.com>
2025-01-11 22:38:53 -0300

    Variáveis e Constantes
    Estes arquivos são um exercício para aprender sobre Variáveis e Constantes, e métodos para nomeá-las em PHP.

--- index.php
+++ index.php
@@ -3,14 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data e Hora</title>
+    <title>Varíaveis e Contantes</title>
 </head>
+
 <body>
-    <h1>Exemplo de PHP</h1>
-    <?php 
-    date_default_timezone_set("America/Sao_Paulo");
-    echo "Hoje é dia " . date("d/M/Y");
-    echo " e a hora é " . date("G:i:s");
-    ?>
+<?php 
+    $nome = "Heitor";
+    $sobrenome = "Antônio";
+
+    const PAIS = "Brasil";
+
+    echo "Muito prazer, $nome $sobrenome! Você mora no ". PAIS . "." ;
+
+?>
+
 </body>
 </html>
+
+<?php 
+//Variáveis sempre começam com $
+//O segundo caractere após $ pode ser letra ou símbolo _
+//É recomendado que Variáveis sejam sempre todas escritas minúsculas e Contantes sejam sempre escritas todas maiúsculas
+?>
+ 
+
+
---
+++ index2.php
@@ -0,0 +1,8 @@
+<?php 
+    $nomeCompletoCliente = "João da Silva";
+    $telefone_contato_cliente = "4002-8933";
+
+//Primeiro método --> camelCase
+//Segundo método --> Snake_Case
+
+?>
